0a436b77e6c4ac409532def515c6befc8160c60a
JonHJul <jonhjul@gmail.com>
2016-03-26 17:40:36 +0000

    Flap animation

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -1,85 +1,91 @@
-
 window.Game = (function() {
-	'use strict';
+    'use strict';
 
-	/**
-	 * Main game class.
-	 * @param {Element} el jQuery element containing the game.
-	 * @constructor
-	 */
-	var Game = function(el) {
-		this.el = el;
-		this.player = new window.Player(this.el.find('.Player'), this);
-		this.isPlaying = false;
+    /**
+     * Main game class.
+     * @param {Element} el jQuery element containing the game.
+     * @constructor
+     */
+    var Game = function(el) {
+        this.el = el;
+        this.player = new window.Player(this.el.find('.Player'), this);
+        this.isPlaying = false;
 
-		// Cache a bound onFrame since we need it each frame.
-		this.onFrame = this.onFrame.bind(this);
-	};
+        var fontSize = Math.min(
+            window.innerWidth / 102.4,
+            window.innerHeight /  57.6
+        );
+        el.css('fontSize', fontSize + 'px');
 
-	/**
-	 * Runs every frame. Calculates a delta and allows each game
-	 * entity to update itself.
-	 */
-	Game.prototype.onFrame = function() {
-		// Check if the game loop should stop.
-		if (!this.isPlaying) {
-			return;
-		}
 
-		// Calculate how long since last frame in seconds.
-		var now = +new Date() / 1000,
-				delta = now - this.lastFrame;
-		this.lastFrame = now;
+        // Cache a bound onFrame since we need it each frame.
+        this.onFrame = this.onFrame.bind(this);
+    };
 
-		// Update game entities.
-		this.player.onFrame(delta);
+    /**
+     * Runs every frame. Calculates a delta and allows each game
+     * entity to update itself.
+     */
+    Game.prototype.onFrame = function() {
+        // Check if the game loop should stop.
+        if (!this.isPlaying) {
+            return;
+        }
 
-		// Request next frame.
-		window.requestAnimationFrame(this.onFrame);
-	};
+        // Calculate how long since last frame in seconds.
+        var now = +new Date() / 1000,
+            delta = now - this.lastFrame;
+        this.lastFrame = now;
 
-	/**
-	 * Starts a new game.
-	 */
-	Game.prototype.start = function() {
-		this.reset();
+        // Update game entities.
+        this.player.onFrame(delta);
 
-		// Restart the onFrame loop
-		this.lastFrame = +new Date() / 1000;
-		window.requestAnimationFrame(this.onFrame);
-		this.isPlaying = true;
-	};
+        // Request next frame.
+        window.requestAnimationFrame(this.onFrame);
+    };
 
-	/**
-	 * Resets the state of the game so a new game can be started.
-	 */
-	Game.prototype.reset = function() {
-		this.player.reset();
-	};
+    /**
+     * Starts a new game.
+     */
+    Game.prototype.start = function() {
+        this.reset();
 
-	/**
-	 * Signals that the game is over.
-	 */
-	Game.prototype.gameover = function() {
-		this.isPlaying = false;
+        // Restart the onFrame loop
+        this.lastFrame = +new Date() / 1000;
+        window.requestAnimationFrame(this.onFrame);
+        this.isPlaying = true;
+    };
 
-		// Should be refactored into a Scoreboard class.
-		var that = this;
-		var scoreboardEl = this.el.find('.Scoreboard');
-		scoreboardEl
-			.addClass('is-visible')
-			.find('.Scoreboard-restart')
-				.one('click', function() {
-					scoreboardEl.removeClass('is-visible');
-					that.start();
-				});
-	};
+    /**
+     * Resets the state of the game so a new game can be started.
+     */
+    Game.prototype.reset = function() {
+        this.player.reset();
+    };
 
-	/**
-	 * Some shared constants.
-	 */
-	Game.prototype.WORLD_WIDTH = 102.4;
-	Game.prototype.WORLD_HEIGHT = 57.6;
+    /**
+     * Signals that the game is over.
+     */
+    Game.prototype.gameover = function() {
+        this.isPlaying = false;
 
-	return Game;
+        // Should be refactored into a Scoreboard class.
+        var that = this;
+        var scoreboardEl = this.el.find('.Scoreboard');
+        scoreboardEl
+            .addClass('is-visible')
+            .find('.Scoreboard-restart')
+            .one('click', function() {
+                scoreboardEl.removeClass('is-visible');
+                that.start();
+            });
+    };
+
+    /**
+     * Some shared constants.
+     */
+    Game.prototype.WORLD_WIDTH = 102.4;
+    Game.prototype.WORLD_HEIGHT = 57.6;
+
+    return Game;
 })();
